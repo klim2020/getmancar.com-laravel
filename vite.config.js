0fd52d9d349ed50f3eef559343a296099fc06885
klim2020 <klimprograms@gmail.com>
2022-08-23 18:46:38 +0300

    initial-commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import {viteStaticCopy} from "vite-plugin-static-copy";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'resources/assets/fonts/**',
+                    dest: 'fonts'
+                },
+                {
+                    src: 'resources/assets/vendor/**',
+                    dest: 'vendor'
+                },
+                {
+                    src: 'resources/assets/images/**',
+                    dest: 'images'
+                }
+            ]
+        }),
+    ],
+});
