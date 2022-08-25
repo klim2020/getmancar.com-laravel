a81f2814e073514ddfbaf93f5e93efe25fc0b365
klim2020 <klimprograms@gmail.com>
2022-08-25 04:51:11 +0300

    --clear route cache

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class TestController extends Controller
 {
     //
     function test(Request $request){
+
+        Cache::flush();
         return view('pages/mainpage');
     }
 }
